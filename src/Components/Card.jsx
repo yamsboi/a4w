e9b83a56887dd9c042a59aae2d2f4822b7c815fc
Yams <yams@MacBook-Pro.local>
2019-07-19 19:20:36 +0200

    Added option to pass custom h1-color in Card-component

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import "./style/card.scss";
 
 import Typography from "./Typography";
+import { removePropertiesDeep } from "@babel/types";
 
 function Card(props) {
   return (
     <div className="card-container">
       <div className="top">
-        <Typography
-          type="h1"
-          style={{ backgroundColor: props.bg, color: props.text }}
-        >
+        <Typography type="h1" style={{ color: props.h1Color }}>
           {props.title}
         </Typography>
 
