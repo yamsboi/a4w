ca1b0f3a4417c3934074718c27f39cb23b76d5fd
seberm <sebastian.ermilin@gmail.com>
2019-07-19 18:11:23 +0200

    bla bla

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style/footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>Sitemap</div>
+      <div>social medias</div>
+    </div>
+  );
+};
+export default Footer;
