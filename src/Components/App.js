11559dc8ee9c676f51a46aecf6075df4ad80214f
seberm <sebastian.ermilin@gmail.com>
2019-07-20 13:06:51 +0200

    major bug fixes, also moved hamburger to bottom right for better usability

--- src/Components/App.js
+++ src/Components/App.js
@@ -13,11 +13,11 @@ function App() {
         <section className="main">
           <Nav />
 
-          <section className="content">
+          <main className="content">
             <Route path="/" exact component={Index} />
             <Route path="/about/" component={About} />
             <Route path="/components/" component={Components} />
-          </section>
+          </main>
         </section>
         <Footer />
       </Router>
