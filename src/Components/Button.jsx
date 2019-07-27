66bbf50a8ae3c347ef76bc4306838eba0a26971c
seberm <sebastian.ermilin@gmail.com>
2019-07-27 12:32:02 +0200

    Responsive changes, >mobile nav

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -12,6 +12,7 @@ const Button = props => {
             d="M4,11V13H16L10.5,18.5L11.92,19.92L19.84,12L11.92,4.08L10.5,5.5L16,11H4Z"
           />
         </svg>
+        <div className="button-shadow" />
       </button>
     </div>
   );
