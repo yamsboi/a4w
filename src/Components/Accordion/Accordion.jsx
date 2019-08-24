9ad469f70c69a458763125210887947c4452e001
seberm <sebastian.ermilin@gmail.com>
2019-08-24 13:02:28 +0200

    Accordion is now super-smooth

--- src/Components/Accordion/Accordion.jsx
+++ src/Components/Accordion/Accordion.jsx
@@ -17,39 +17,47 @@ function Accordion({ children }) {
     }
   }
 
-  return React.Children.map(children, (child, i) => (
-    <div className="accordion__section" key={i}>
-      <button
-        className={"accordion"}
-        onClick={() => setItem(child.props.title)}>
-        <Typography className="accordion__title" type="headline">
-          {child.props.title}
-        </Typography>
-        <Chevron
-          className={
-            activeItem === child.props.title
-              ? "accordion__icon rotate"
-              : "accordion__icon"
-          }
-          width={10}
-          fill={"#000"}
-        />
-      </button>
-      <div
-        ref={content}
-        className="accordion__content"
-        style={{
-          maxHeight:
-            activeItem === child.props.title
-              ? `${content.current.scrollHeight}px`
-              : "0px"
-        }}>
-        <div className="accordion__text">
-          <div className="accordion__children">{child}</div>
+  return (
+    <div className="accordion__container">
+      {React.Children.map(children, (child, i) => (
+        <div
+          className={`accordion__section ${
+            activeItem === child.props.title ? "accordion__active" : ""
+          }`}
+          key={i}>
+          <button
+            className={"accordion"}
+            onClick={() => setItem(child.props.title)}>
+            <Typography className="accordion__title" type="headline">
+              {child.props.title}
+            </Typography>
+            <Chevron
+              className={
+                activeItem === child.props.title
+                  ? "accordion__icon rotate"
+                  : "accordion__icon"
+              }
+              width={10}
+              fill={"#000"}
+            />
+          </button>
+          <div
+            ref={content}
+            className="accordion__content"
+            style={{
+              maxHeight:
+                activeItem === child.props.title
+                  ? `${content.current.scrollHeight}px`
+                  : "0px"
+            }}>
+            <div className="accordion__text">
+              <div className="accordion__children">{child}</div>
+            </div>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
-  ));
+  );
 }
 
 export default Accordion;
