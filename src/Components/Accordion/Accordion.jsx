bae767276bdb365553029a682e4c226def9d57cd
seberm <sebastian.ermilin@gmail.com>
2019-08-24 13:23:30 +0200

    Accordion update

--- src/Components/Accordion/Accordion.jsx
+++ src/Components/Accordion/Accordion.jsx
@@ -18,45 +18,47 @@ function Accordion({ children }) {
   }
 
   return (
-    <div className="accordion__container">
-      {React.Children.map(children, (child, i) => (
-        <div
-          className={`accordion__section ${
-            activeItem === child.props.title ? "accordion__active" : ""
-          }`}
-          key={i}>
-          <button
-            className={"accordion"}
-            onClick={() => setItem(child.props.title)}>
-            <Typography className="accordion__title" type="headline">
-              {child.props.title}
-            </Typography>
-            <Chevron
-              className={
-                activeItem === child.props.title
-                  ? "accordion__icon rotate"
-                  : "accordion__icon"
-              }
-              width={10}
-              fill={"#000"}
-            />
-          </button>
-          <div
-            ref={content}
-            className="accordion__content"
-            style={{
-              maxHeight:
-                activeItem === child.props.title
-                  ? `${content.current.scrollHeight}px`
-                  : "0px"
-            }}>
-            <div className="accordion__text">
-              <div className="accordion__children">{child}</div>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
+    <table className="accordion__container">
+      <tbody>
+        {React.Children.map(children, (child, i) => (
+          <tr
+            className={`accordion__section ${
+              activeItem === child.props.title ? "accordion__active" : ""
+            }`}
+            key={i}>
+            <th
+              className={"accordion"}
+              onClick={() => setItem(child.props.title)}>
+              <Typography className="accordion__title" type="headline">
+                {child.props.title}
+              </Typography>
+              <Chevron
+                className={
+                  activeItem === child.props.title
+                    ? "accordion__icon rotate"
+                    : "accordion__icon"
+                }
+                width={10}
+                fill={"#000"}
+              />
+            </th>
+            <td
+              ref={content}
+              className="accordion__content"
+              style={{
+                maxHeight:
+                  activeItem === child.props.title
+                    ? `${content.current.scrollHeight}px`
+                    : "0px"
+              }}>
+              <div className="accordion__text">
+                <div className="accordion__children">{child}</div>
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
